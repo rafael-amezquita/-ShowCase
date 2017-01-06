0874c21fcce956e04b60d4398472afb63b956e48
ramezqui <rafael.x.amezquita.-nd@disney.com>
2017-01-06 17:02:02 -0500

    project structure

---
+++ AppsShowcase/Modules/Core/CategoriesFacade.swift
@@ -0,0 +1,9 @@
+//
+//  CategoriesFacade.swift
+//  AppsShowcase
+//
+//  Created by Rafael Amezquita on 6/01/17.
+//  Copyright © 2017 Rafael Amezquita. All rights reserved.
+//
+
+import Foundation
---
+++ AppsShowcase/Modules/Core/ShowcaseFacade.swift
@@ -0,0 +1,9 @@
+//
+//  ShowcaseFacade.swift
+//  Showcase
+//
+//  Created by Rafael Amezquita on 6/01/17.
+//  Copyright © 2017 Rafael Amezquita. All rights reserved.
+//
+
+import Foundation
